5bf0b011ee3b3ff3b9b38b1f98c46c3f66d3633b
SplendidX <splendidx@cocaine.ninja>
2019-01-19 21:31:39 +0200

    escript added

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Fortress.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: Fortress.CLI],
       deps: deps()
     ]
   end
